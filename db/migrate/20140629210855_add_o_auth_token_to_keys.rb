9751c9e6b3dcfc80e26f172754d94ed48a723a74
Kyle Boss <kyle@kyleboss.com>
2014-07-07 21:23:09 -0700

    Switched to PG instead of SQLite

--- db/migrate/20140629210855_add_o_auth_token_to_keys.rb
+++
@@ -1,5 +0,0 @@
-class AddOAuthTokenToKeys < ActiveRecord::Migration
-  def change
-    add_column :keys, :oauth_token, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140629210855) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "keys", force: true do |t|
-    t.string   "consumer_key"
-    t.string   "consumer_secret"
-    t.string   "oauth_token_secret"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "oauth_token"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
