a05926cf1bca73ff21825dfe4f2f70ac0fc0de02
Kyle Boss <kyle@kylethewizkid.com>
2014-08-10 12:58:09 -0700

    Added header. UI'd inputs. Video background

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Twitsus
   class Application < Rails::Application
+    Rails.root.join('app', 'assets', 'videos')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
